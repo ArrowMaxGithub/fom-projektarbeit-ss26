80c2bef2dbc886162bf9185a12daa22d5c2e3070
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2026-03-30 18:38:58 +0200

    Add default human vs bot game

--- durak-bot/src/main.py
+++ durak-bot/src/main.py
@@ -1,8 +1,23 @@
+from bot_lowest_card import BotLowestCard
+from human_terminal_player import HumanTerminalPlayer
+from terminal_output import TerminalOutput
+from two_player_game import TwoPlayerGame
+
 # TODO: CLI parse arguments to setup different variants: Player count, Human vs Bot, Bot vs Bot
-# TODO: Start the default game: assume a two player game human vs bot_lowest_card on a terminal
 
 
-def main(): ...
+def main():
+    # Start the default game: assume a two player game human vs bot_lowest_card on a terminal
+    player = HumanTerminalPlayer()
+    opponent = BotLowestCard()
+    game = TwoPlayerGame()
+    output = TerminalOutput()
+
+    game.RegisterOutputCallback(output.OnRender)
+    player.RegisterActionCallback(game.OnAction)
+    opponent.RegisterActionCallback(game.OnAction)
+
+    game.Start([player, opponent])
 
 
 if __name__ == "__main__":
