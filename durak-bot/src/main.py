d6ced8d1f31dba0f1c43ec88b2e2c27254ea726c
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2026-04-20 18:37:40 +0200

    Add CLI parser

--- durak-bot/src/main.py
+++ durak-bot/src/main.py
@@ -1,20 +1,62 @@
+import typer
+from enum import Enum
+from typing import Annotated
+
 from bot_lowest_card import BotLowestCard
 from human_terminal_player import HumanTerminalPlayer
 from terminal_output import TerminalOutput
 from two_player_game import TwoPlayerGame
 
-# TODO: CLI parse arguments to setup different variants: Player count, Human vs Bot, Bot vs Bot
+app = typer.Typer()
 
 
-def main():
-    # Start the default game: assume a two player game human vs bot_lowest_card on a terminal
+class Bot(str, Enum):
+    LowestCard = "bot-lowest-card"
+
+
+class Ui(str, Enum):
+    Terminal = "terminal"
+
+
+@app.command()
+def play(
+    bot: Annotated[
+        Bot,
+        typer.Argument(help="The name of the bot to play against."),
+    ] = "bot-lowest-card",
+    ui: Annotated[Ui, typer.Argument(help="Selected interface type.")] = "terminal",
+):
+    """
+    Play a two-player game against a selected bot.
+    """
+    match bot:
+        case Bot.LowestCard:
+            opponent = BotLowestCard()
+
+    match ui:
+        case Ui.Terminal:
+            output = TerminalOutput()
+
     player = HumanTerminalPlayer()
-    opponent = BotLowestCard()
     game = TwoPlayerGame()
-    output = TerminalOutput()
-
     game.Start([player, opponent], output)
 
 
+@app.command()
+def train():
+    """
+    Train a bot with the provided parameters.
+    """
+    ...
+
+
+@app.command()
+def simulate():
+    """
+    Simulate X number of games between two bots.
+    """
+    ...
+
+
 if __name__ == "__main__":
-    main()
+    app()
